3e12caa2e580ba70862c2ddffb6e45069e03281f
SakshaGotad <sakshagotad@gmai.com>
2024-12-27 10:39:35 +0530

    frontend changes

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/user-routes');
 const queryRoutes = require('./routes/query-routes');
 const answerRoutes = require('./routes/answer-routes');
 app.use(cors({
-    origin: 'https://superlative-fairy-626ab3.netlify.app', 
+    origin: 'http://localhost:5173/', 
   credentials: true,
 }));
 app.use(express.json());
